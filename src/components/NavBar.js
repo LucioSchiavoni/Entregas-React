11be066f10fcb1352910a0092f373e4ebbc29e2b
Lucio Schiavoni <104010031+LucioSchiavoni@users.noreply.github.com>
2022-08-30 21:31:52 -0300

    Entrega

---
+++ src/components/NavBar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+
+
+
+export const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Inicio</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Productos</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+
+export default NavBar
